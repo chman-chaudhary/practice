5187f7d71d00e46524f1ede598806e801aeafba5
Chaman Chaudhary <chaudharychaman1506@gmail.com>
2024-12-16 22:24:30 +0530

    making scroll text animation

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-const main = document.querySelector(".main");
-const cursor = document.querySelector(".cursor");
-const open = document.querySelector(".open");
-const close = document.querySelector(".close");
-const sidebar = document.querySelector(".sidebar");
-const li = document.getElementsByTagName("li");
-
-const tl = gsap.timeline();
-
-tl.to(sidebar, {
-  x: 0,
-  duration: 0.6,
-});
-
-tl.from(li, {
-  x: 20,
-  opacity: 0,
-  duration: 0.3,
-  stagger: 0.3,
-});
-
-tl.pause();
-
-main.addEventListener("mousemove", (dets) => {
-  const { x, y } = dets;
-
-  gsap.to(cursor, {
-    x,
-    y,
-    duration: 1,
-    ease: "back.out",
-  });
-});
-
-open.addEventListener("click", () => {
-  tl.play();
-});
-
-close.addEventListener("click", () => {
-  tl.reverse();
+window.addEventListener("wheel", (event) => {
+  if (event.deltaY >= 0) {
+    gsap.to("img", { rotate: 180 });
+    gsap.to(".marque", {
+      x: "-200%",
+      duration: 4,
+      repeat: -1,
+      ease: "none",
+    });
+  } else {
+    gsap.to("img", { rotate: 0 });
+    gsap.to(".marque", {
+      x: "0%",
+      duration: 4,
+      repeat: -1,
+      ease: "none",
+    });
+  }
 });
