f5f56a2754fa025a2a281221d52dbcd6ffbdc3b5
Chaman Chaudhary <chaudharychaman1506@gmail.com>
2024-12-17 15:56:14 +0530

    learning timeline

--- script.js
+++ script.js
@@ -1,19 +1,92 @@
-window.addEventListener("wheel", (event) => {
-  if (event.deltaY >= 0) {
-    gsap.to("img", { rotate: 180 });
-    gsap.to(".marque", {
-      x: "-200%",
-      duration: 4,
-      repeat: -1,
-      ease: "none",
-    });
-  } else {
-    gsap.to("img", { rotate: 0 });
-    gsap.to(".marque", {
-      x: "0%",
-      duration: 4,
-      repeat: -1,
-      ease: "none",
-    });
-  }
+let tl1 = gsap.timeline();
+
+tl1.from(".logo, .navlinks li", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  delay: 1,
+  stagger: 0.2,
 });
+
+tl1.from(".hero h1, .hero p, .hero button", {
+  x: -200,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+});
+
+tl1.from(".partners li", {
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+});
+
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".service-items",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 0",
+    scrub: 2,
+  },
+});
+
+tl2.from(
+  ".line1.left",
+  {
+    x: -300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step1"
+);
+
+tl2.from(
+  ".line1.right",
+  {
+    x: 300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step1"
+);
+
+tl2.from(
+  ".line2.left",
+  {
+    x: -300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step2"
+);
+
+tl2.from(
+  ".line2.right",
+  {
+    x: 300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step2"
+);
+tl2.from(
+  ".line3.left",
+  {
+    x: -300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step3"
+);
+
+tl2.from(
+  ".line3.right",
+  {
+    x: 300,
+    opacity: 0,
+    duration: 0.5,
+  },
+  "step3"
+);
